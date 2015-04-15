c3ede0caed18a00cb381c6ff5d337fa08f592775
Elliot Ford <ElliotFord@live.co.uk>
2015-04-15 22:35:11 +0100

    Stream update to Taggable

--- JALSE/src/jalse/tags/Taggable.java
+++ JALSE/src/jalse/tags/Taggable.java
@@ -1,6 +1,7 @@
 package jalse.tags;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Used identify any class that is exposing internal state through {@link Tag}.
@@ -16,4 +17,13 @@ public interface Taggable {
      * @return All tags or an empty set if none are found.
      */
     Set<Tag> getTags();
+
+    /**
+     * Streams all tag state information.
+     * 
+     * @return Stream of all tags.
+     */
+    default Stream<Tag> streamTags() {
+	return getTags().stream();
+    }
 }
