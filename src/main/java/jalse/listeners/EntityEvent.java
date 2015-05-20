e26c8df7510c9e215e82123f7c8a19e41b16dd18
Neal Miller <neal.miller@rigminder.com>
2015-05-19 21:21:50 -0500

    Added entity type changes to EntityListener.

--- src/main/java/jalse/listeners/EntityEvent.java
+++ src/main/java/jalse/listeners/EntityEvent.java
@@ -21,6 +21,7 @@ public class EntityEvent extends AbstractIdentifiable {
 
     private final Entity entity;
     private final EntityContainer container;
+    private final Class<? extends Entity> typeChange;
     private final EntityContainer destination;
 
     /**
@@ -32,7 +33,42 @@ public class EntityEvent extends AbstractIdentifiable {
      *            Entity the event is for.
      */
     public EntityEvent(final EntityContainer container, final Entity entity) {
-	this(container, entity, null);
+	this(container, entity, null, null);
+    }
+
+    /**
+     * Creates a new EntityEvent with a random ID.
+     *
+     * @param container
+     *            Parent container for the Entity.
+     * @param entity
+     *            Entity the event is for.
+     * @param typeChange
+     *            The type the entity was changed to.
+     */
+    public EntityEvent(final EntityContainer container, final Entity entity, final Class<? extends Entity> typeChange) {
+	this(container, entity, typeChange, null);
+    }
+
+    /**
+     * Creates a new EntityEvent with a random ID.
+     *
+     * @param container
+     *            Parent container for the entity.
+     * @param entity
+     *            Entity the event is for.
+     * @param typeChange
+     *            The type the entity was changed to.
+     * @param destination
+     *            Destination container for transfer events (can be null).
+     */
+    public EntityEvent(final EntityContainer container, final Entity entity, final Class<? extends Entity> typeChange,
+	    final EntityContainer destination) {
+	super(UUID.randomUUID());
+	this.container = Objects.requireNonNull(container);
+	this.entity = Objects.requireNonNull(entity);
+	this.typeChange = typeChange;
+	this.destination = destination;
     }
 
     /**
@@ -46,10 +82,7 @@ public class EntityEvent extends AbstractIdentifiable {
      *            Destination container for transfer events (can be null).
      */
     public EntityEvent(final EntityContainer container, final Entity entity, final EntityContainer destination) {
-	super(UUID.randomUUID());
-	this.container = Objects.requireNonNull(container);
-	this.entity = Objects.requireNonNull(entity);
-	this.destination = destination;
+	this(container, entity, null, destination);
     }
 
     /**
@@ -90,6 +123,25 @@ public class EntityEvent extends AbstractIdentifiable {
     }
 
     /**
+     * Get the type the entity was changed to.
+     *
+     * @return Optional containing the type the entity was changed to or empty optional if the type
+     *         was not changed.
+     */
+    public Optional<Class<? extends Entity>> getOptTypeChange() {
+	return Optional.ofNullable(typeChange);
+    }
+
+    /**
+     * Get the type the entity was changed to.
+     *
+     * @return The type the entity was changed to.
+     */
+    public Class<? extends Entity> getTypeChange() {
+	return typeChange;
+    }
+
+    /**
      * Checks whether this is a transfer event.
      *
      * @return Whether there is a destination container.
@@ -97,4 +149,14 @@ public class EntityEvent extends AbstractIdentifiable {
     public boolean isTransfer() {
 	return destination != null;
     }
+
+    /**
+     * Checks whether this is a type change event.
+     *
+     * @return Whether the type was changed.
+     */
+    public boolean isTypeChange() {
+	return getTypeChange() != null;
+    }
+
 }
