3e22d6eb4000901a98ca2d7370c88a0f4cd901d1
Elliot Ford <ElliotFord@live.co.uk>
2015-06-02 00:47:44 +0100

    Default method performance improvements

--- src/main/java/jalse/entities/methods/DefaultMethod.java
+++ src/main/java/jalse/entities/methods/DefaultMethod.java
@@ -3,12 +3,14 @@ package jalse.entities.methods;
 import jalse.entities.Entity;
 import jalse.entities.functions.DefaultFunction;
 
-import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.reflect.Method;
+import java.lang.invoke.MethodHandle;
 import java.util.Objects;
 
 /**
- * This is used for mapping {@code default} methods.
+ * This is used for mapping {@code default} methods.<br>
+ * <br>
+ * It is worth noting that this class has been designed with performance in mind. Default method
+ * invocation on a dynamic proxy is quite problematic and slow (for now).
  *
  * @author Elliot Ford
  *
@@ -16,33 +18,71 @@ import java.util.Objects;
  */
 public class DefaultMethod implements EntityMethod {
 
-    private final Method m;
-    private final Lookup lookup;
+    private final MethodHandle handle;
+    private final int argCount;
 
     /**
      * Creates a new default method.
      *
-     * @param m
-     *            Method to invoke.
-     * @param lookup
-     *            Method class lookup.
+     * @param handle
+     *            Method handle to invoke.
+     * @param argCount
+     *            Method parameter count.
      */
-    public DefaultMethod(final Method m, final Lookup lookup) {
-	this.m = Objects.requireNonNull(m);
-	this.lookup = Objects.requireNonNull(lookup);
+    public DefaultMethod(final MethodHandle handle, final int argCount) {
+	this.handle = Objects.requireNonNull(handle);
+	if (argCount < 0) {
+	    throw new IllegalArgumentException();
+	}
+	this.argCount = argCount;
     }
 
     /**
-     * Gets the method to invoke.
+     * Gets the method argument count.
      *
-     * @return Type method.
+     * @return Argument count.
      */
-    public Method getMethod() {
-	return m;
+    public int getArgCount() {
+	return argCount;
+    }
+
+    /**
+     * Gets the method handle to invoke.
+     *
+     * @return Method handle.
+     */
+    public MethodHandle getHandle() {
+	return handle;
     }
 
     @Override
     public Object invoke(final Object proxy, final Entity e, final Object[] args) throws Throwable {
-	return lookup.unreflectSpecial(m, m.getDeclaringClass()).bindTo(proxy).invokeWithArguments(args);
+	/*
+	 * Generally the maximum accepted parameter count for a method is 7.
+	 */
+	switch (argCount) {
+	case 0:
+	    return handle.invoke(proxy);
+	case 1:
+	    return handle.invoke(proxy, args[0]);
+	case 2:
+	    return handle.invoke(proxy, args[0], args[1]);
+	case 3:
+	    return handle.invoke(proxy, args[0], args[1], args[2]);
+	case 4:
+	    return handle.invoke(proxy, args[0], args[1], args[2], args[3]);
+	case 5:
+	    return handle.invoke(proxy, args[0], args[1], args[2], args[3], args[4]);
+	case 6:
+	    return handle.invoke(proxy, args[0], args[1], args[2], args[3], args[4], args[5]);
+	case 7:
+	    return handle.invoke(proxy, args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+	default:
+	    /*
+	     * This has a huge performance hit!
+	     */
+	    return handle.bindTo(proxy).invokeWithArguments(args);
+	}
     }
+
 }
